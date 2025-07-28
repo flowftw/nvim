ee47d244148fe202b12b44494b34ed1de2b73f5b
Florian Schumann <florian.schumann@mailbox.org>
2025-07-28 21:11:40 +0200

    feat(lazy|colors): update

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -18,6 +18,12 @@ return {
         "catppuccin/nvim",
         name = "catppuccin",
         priority = 1000,
+        config = function()
+            require("catppuccin").setup({
+                no_italic = true
+            })
+            vim.cmd("colorscheme catppuccin")
+        end
     },
     -- {
     --     "shaunsingh/nord.nvim",
