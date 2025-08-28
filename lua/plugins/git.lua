1c43422762ff42c89c37245f97a77ecb8627dff9
Florian Schumann <florian.schumann@cardanofoundation.org>
2025-08-28 17:58:13 +0200

    feat: add gitlinker

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -10,9 +10,9 @@ return {
             "echasnovski/mini.pick",         -- optional
             'lewis6991/gitsigns.nvim'
         },
-        config = function ()
+        config = function()
             require('gitsigns').setup({
-                on_attach = function (bufnr)
+                on_attach = function(bufnr)
                     local gitsigns = require("gitsigns")
                     vim.keymap.set('n', '<leader>hp', gitsigns.preview_hunk)
                     vim.keymap.set('n', '<leader>hr', gitsigns.reset_hunk)
@@ -31,12 +31,21 @@ return {
                 }
             })
             vim.keymap.set('n', '<leader>gg', "<cmd>Neogit<cr>", {})
-            vim.keymap.set('n', '<leader>gl', function ()
+            vim.keymap.set('n', '<leader>gl', function()
                 require("neogit").action("log", "log_current", {})()
             end, {})
             vim.keymap.set('n', '<leader>gdd', "<cmd>DiffviewOpen origin/dev...HEAD<cr>", {})
             vim.keymap.set('n', '<leader>gdm', "<cmd>DiffviewOpen origin/main...HEAD<cr>", {})
             vim.keymap.set('n', '<leader>gdc', "<cmd>DiffviewClose<cr>", {})
         end
+    },
+    {
+        "linrongbin16/gitlinker.nvim",
+        cmd = "GitLink",
+        opts = {},
+        keys = {
+            { "<leader>gy", "<cmd>GitLink<cr>",  mode = { "n", "v" }, desc = "Yank git link" },
+            { "<leader>gY", "<cmd>GitLink!<cr>", mode = { "n", "v" }, desc = "Open git link" },
+        },
     }
 }
