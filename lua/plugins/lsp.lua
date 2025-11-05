f7a27ada0ec332af0d9ffc583ab74f1eed78e1c2
Florian Schumann <florian.schumann@cardanofoundation.org>
2025-11-05 16:33:42 +0100

    fix(jdtls): disable mason-lspconfig for jdtls

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -95,6 +95,11 @@ return {
 
             require('mason-lspconfig').setup({
                 ensure_installed = { 'jdtls' },
+                automatic_enable = {
+                    exclude = {
+                        "jdtls"
+                    }
+                }
             })
 
             vim.lsp.config('lua_ls', {})
@@ -122,7 +127,55 @@ return {
                 }
             })
 
-            vim.lsp.config('jdtls', {})
+            -- TODO: maybe 
+            -- local root_dir = vim.fs.root(0, { '.git', { 'build.gradle.kts', 'build.gradle', '.gradlew', '.mvnw' } })
+            -- local home = os.getenv('HOME')
+            -- local workspace_folder = home .. "/.local/share/eclipse/" .. vim.fn.fnamemodify(root_dir, ":p:h:t")
+            -- local install_path = home .. "/.local/share/nvim/mason/packages/jdtls/"
+            -- vim.lsp.config("jdtls", {
+            --     settings = {
+            --         java = {
+            --             -- Custom eclipse.jdt.ls options go here
+            --             cmd = {
+            --                 -- 💀
+            --                 '/Library/Java/JavaVirtualMachines/temurin-21.jdk/Contents/Home/bin/java', -- or '/path/to/java21_or_newer/bin/java'
+            --                 -- depends on if `java` is in your $PATH env variable and if it points to the right version.
+            --
+            --                 '-Declipse.application=org.eclipse.jdt.ls.core.id1',
+            --                 '-Dosgi.bundles.defaultStartLevel=4',
+            --                 '-Declipse.product=org.eclipse.jdt.ls.core.product',
+            --                 '-Dlog.protocol=true',
+            --                 '-Dlog.level=ALL',
+            --                 '-Xmx1g',
+            --                 '--add-modules=ALL-SYSTEM',
+            --                 '--add-opens', 'java.base/java.util=ALL-UNNAMED',
+            --                 '--add-opens', 'java.base/java.lang=ALL-UNNAMED',
+            --                 "-javaagent:" .. install_path .. "lombok.jar",
+            --
+            --                 -- 💀
+            --                 '-jar', vim.fn.glob(install_path .. 'plugins/org.eclipse.equinox.launcher_*.jar'),
+            --                 -- ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^                                       ^^^^^^^^^^^^^^
+            --                 -- Must point to the                                                     Change this to
+            --                 -- eclipse.jdt.ls installation                                           the actual version
+            --
+            --
+            --                 -- 💀
+            --                 '-configuration', vim.fn.glob(install_path .. 'config_mac_arm'),
+            --                 -- ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^        ^^^^^^
+            --                 -- Must point to the                      Change to one of `linux`, `win` or `mac`
+            --                 -- eclipse.jdt.ls installation            Depending on your system.
+            --
+            --
+            --                 -- 💀
+            --                 -- See `data directory configuration` section in the README
+            --                 '-data', workspace_folder,
+            --             },
+            --             root_dir = vim.fs.root(0, { '.git', { 'build.gradle.kts', 'build.gradle', '.gradlew', '.mvnw' } }),
+            --         },
+            --     },
+            -- })
+            -- vim.lsp.enable("jdtls")
+            --
         end
     },
 }
